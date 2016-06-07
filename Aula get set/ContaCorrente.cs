875148419f626577c8b977fa60a9cf9da8916ee3
Rodrigo Jucá <rodjucateixeira@gmail.com>
2016-06-07 14:12:35 -0300

    Aula get/set

---
+++ Aula get set/ContaCorrente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ContaCorrente
+    {
+        public string NomeCliente { get; set; }
+        private double _saldo;
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                if (value >= 0)
+                    _saldo = value;
+            }
+        }
+        public void Debitar( double valor)
+        {
+            if ( this.Saldo >= valor)
+            {
+                this.Saldo = valor;
+            }
+        }
+
+    }
+}
---
+++ Aula get set/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aula de get/set
+            ContaCorrente c = new ContaCorrente();
+            c.NomeCliente = "Jooj";
+            c.Saldo = 5000;
+            c.Debitar(500);
+            if (c.Saldo >= 500)
+            {
+                Console.WriteLine(c.Saldo);
+            }
+            
+        }
+    }
+}
