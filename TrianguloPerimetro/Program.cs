18d63d02dbedb305d0593665d2a2e6d35172c2da
Rodrigo Jucá <rodjucateixeira@gmail.com>
2016-06-13 20:45:46 -0300

    Exercício FUP

---
+++ TrianguloPerimetro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique o número de triângulos que deseja de entrada");
+            int triangulos = int.Parse(Console.ReadLine());
+            double maiorp = 0; double maiorid = 0; int j = 0;
+            double[] perimetros = new double[triangulos];
+            int[] lados = new int[3 * triangulos];
+            int[] ids = new int[triangulos];
+            int numlados = 3 * triangulos;
+            Console.WriteLine("Indique respectivamente o tamanho dos lados dos triângulos");
+            for (int i = 0; i < numlados; i++)
+			{
+                lados[i] = int.Parse(Console.ReadLine());
+			}
+            Console.WriteLine("Informe respectivamente o ID dos triângulos");
+            for (int i = 0; i < triangulos; i++)
+            {
+                ids[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < numlados; i+=3)
+            { 
+                Triangulo a = new Triangulo(lados[i], lados[i + 1], lados[i + 2]);
+                perimetros[j] = a.Perimetro;
+                
+                if (perimetros[j] > maiorp)
+                    {              
+                        maiorp = perimetros[j];
+                        maiorid = ids[j];
+                    }
+                j++;
+            }
+            Console.WriteLine("o maior perimetro é {0} e o id deste triângulo é {1}", maiorp, maiorid);
+        }
+    }
+}
---
+++ TrianguloPerimetro/Triangulo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Triangulo
+    {
+        private double _perimetro;
+        public double Perimetro 
+        {
+            get {return _perimetro;}
+            set
+            {
+                if (value > 0)
+                    _perimetro = value;
+            }
+        }
+        public int Id { get; set; }
+        private int _cor;
+        public int Cor
+        {
+            get { return _cor; }
+            set
+            {
+                if (_cor >= 0 && _cor < 16777216)
+                    _cor = value;
+            }
+
+        }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        private double _ladoA;
+        public double LadoA
+        {
+            get { return _ladoA; }
+            set
+            {
+                if (value > 0)
+                    _ladoA = value;
+            }
+        }
+        private double _ladoB;
+        public double LadoB
+        {
+            get { return _ladoB; }
+            set 
+            {
+                if (value > 0)
+                    _ladoB = value;
+            }
+        }
+        private double _ladoC;
+        public double LadoC
+        {
+            get { return _ladoC; }
+            set
+            {
+                if (value > 0)
+                    _ladoC = value;
+            }
+        }
+        public Triangulo ( double LadoA , double LadoB , double LadoC)
+        {
+            Perimetro = LadoA + LadoB + LadoC;
+        }
+    }
+}
