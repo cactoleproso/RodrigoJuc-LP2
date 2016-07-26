219fc79935e6095301339ee8ac09b2e8a1319a9d
Rodrigo Jucá <rodjucateixeira@gmail.com>
2016-07-25 23:06:46 -0300

    KEK

---
+++ LojaDeJogos/LojaDeJogos/Adicionar.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LojaDeJogos
+{
+    /// <summary>
+    /// Interaction logic for Adicionar.xaml
+    /// </summary>
+    public partial class Adicionar : Window
+    {
+        MainWindow mw = new MainWindow();
+        public Adicionar()
+        {
+            InitializeComponent();
+            
+        }
+        public void AdicionarID_Click(object sender, RoutedEventArgs e)
+        {
+            int num = int.Parse(mw.NumAdd.Text);
+            Jogo jogo = new Jogo();
+            for (int i = 0; i < num; i++)
+            {
+                
+            }
+        }
+
+        private void PreçoAD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AnoAD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ReqAD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GeneroAD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NomeAD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LojaDeJogos/LojaDeJogos/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LojaDeJogos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void Bt_Add(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Bt_Con(object sender, RoutedEventArgs e)
+        {
+            Consultar con = new Consultar();
+            con.Show();
+            this.IsEnabled = false;
+        }
+
+        private void Bt_Alt(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt_Rem(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void NumAdd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
