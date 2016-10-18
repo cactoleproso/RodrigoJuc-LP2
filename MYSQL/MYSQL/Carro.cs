d97551c58d81d410e8fa754607e530f7090552de
cactoleproso <rodjucateixeira@gmail.com>
2016-10-18 13:12:21 -0300

    Add files via upload

---
+++ MYSQL/MYSQL/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYSQL
+{
+    class Carro
+    {
+        public int id { get; set; }
+        public string Placa { get; set; }
+        public int IdCidade { get; set; }
+        public double PreçoRevenda { get; set; }
+
+    }
+}
