860e987d471813117eddb4b5caa812113a156eea
cactoleproso <rodjucateixeira@gmail.com>
2017-02-14 14:47:02 -0300

    aula 14/02

---
+++ revisao lp2/ConsoleApplication2/ConsoleApplication2/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ revisao lp2/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            Console.Write("Digite nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Digite Sobrenome: ");
+            p.Sobrenome = Console.ReadLine();
+
+            Cadastrar(p); 
+        }
+        static void Cadastrar(Pessoa pessoa)
+        {
+            MySqlConnection conn = new MySqlConnection("Server=localhost;Database=test;Uid=root;Pwd=");
+            MySqlCommand cmd = new MySqlCommand();
+            
+            cmd.Connection = conn;
+            //    OU
+            //   MySqlCommand cmd = new MySqlCommand()
+            //  {
+            //       Connection = new MySqlConnection("Server=...."),
+            //       CommandText = "INSERT INTO aula (nome, sobrenome) VALUES (@nome, @sobrenome);"
+            //
+            //
+            //
+            //
+            cmd.CommandText = "INSERT INTO aula (Nome, Sobrenome) VALUES (@nome, @sobrenome);";
+
+            cmd.Parameters.AddWithValue("@nome", pessoa.Nome);
+            cmd.Parameters.AddWithValue("@sobrenome", pessoa.Sobrenome);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+
+            //cadastra dados
+        }
+    }
+}
