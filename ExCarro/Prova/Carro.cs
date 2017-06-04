f7c389810698cec4b88d0ca17215644f2609d97f
Rodrigo Jucá <rodjucateixeira@gmail.com>
2017-06-04 16:30:16 -0300

    a

--- ExCarro/Prova/Carro.cs
+++ ExCarro/Prova/Carro.cs
@@ -1,29 +1,72 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prova
+namespace TenhoProvaAmanhã
 {
-    public class Carro
+    class Program
     {
-        private int _velocidade;
-        public int Velocidade
+        static void Main(string[] args)
         {
-            get { return _velocidade;  }
-            set
+            Pessoa p = new Pessoa();
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Sobrenome: ");
+            p.Sobrenome = Console.ReadLine();
+            Cadastrar(p);
+
+            foreach (Pessoa P in Consulta())
             {
-                if (value > 0)
-                    _velocidade = value;
+                Console.WriteLine(" Id = {0}\n Nome = {1}\n Sobrenome = {2}", p.Id, p.Nome, p.Sobrenome);
             }
+
         }
-        
-        public string Modelo { get; set; }
-        public Carro()
+
+        static void Cadastrar(Pessoa p)
         {
-            this.Velocidade = Velocidade;
-            this.Modelo = Modelo;
+            MySqlCommand cmd = new MySqlCommand()
+            {
+                Connection = new MySqlConnection("Server=localhost;Database=turma;Uid=root;Pwd="),
+                CommandText = "INSERT INTO Pessoa (Nome, Sobrenome) VALUES (@nome, @sobrenome);"
+            };
+
+            cmd.Parameters.AddWithValue("@nome", p.Nome);
+            cmd.Parameters.AddWithValue("@sobrenome", p.Sobrenome);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+        }
+
+        public static List<Pessoa> Consulta()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            MySqlCommand cmd = new MySqlCommand()
+            {
+                Connection = new MySqlConnection("Server=localhost;Database=turma;Uid=root;Pwd="),
+                CommandText = "SELECT * FROM Pessoa"
+            };
+
+            cmd.Connection.Open();
+            MySqlDataReader result = cmd.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Pessoa p = new Pessoa();
+                    p.Id = result.GetInt32(0);
+                    p.Nome = result.GetString(1);
+                    p.Sobrenome = result.GetString(2);
+                    pessoas.Add(p);
+                }
+            }
+            cmd.Connection.Close();
+            return pessoas;
         }
     }
 }
